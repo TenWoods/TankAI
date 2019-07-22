1670b1a1f6cdd6087a77fb65140be9829d96a432
じゅうき <31563826+TenWoods@users.noreply.github.com>
2019-07-22 17:47:19 +0800

    init

---
+++ Assets/TanksMultiplayer/Scripts/TenWoodsAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TanksMP
+{
+    public class TenWoodsAI : BaseControl
+    {
+        GameObject targetItem = null;
+        BasePlayer targetPlayer = null;
+
+        protected override void OnUpdate()
+        {
+            base.OnUpdate();
+            if (targetItem == null)
+            {
+                GameObject[] allItem = GameObject.FindGameObjectsWithTag("Powerup");
+                targetItem = allItem[Random.Range(0, allItem.Length)];
+            }
+            if (targetItem != null)
+            {
+                tankPlayer.MoveTo(targetItem.transform.position);
+            }
+        }
+    }
+}
